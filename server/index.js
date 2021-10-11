890aac3f60b9f9a52468990a424c9ffd761ddf6d
keshav263 <chhapariakeshav@gmail.com>
2021-10-11 22:55:40 +0530

    add try/catch server

--- server/index.js
+++ server/index.js
@@ -23,49 +23,54 @@ app.post("/scrape-reviews", (req, res) => {
 	if (!url) {
 		return res.status(400).send({ message: "URL Required" });
 	}
-	const main = spawn("python", ["scraper.py", url]);
-	main.stdout.on("data", function (data) {
-		const logistic = spawn("python", ["logistic.py"]);
-		logistic.stdout.on("data", function (data) {
-			console.log("Pipe data from python script ...");
-			let lr = JSON.parse(data.toString());
-			const randomForest = spawn("python", ["rf.py"]);
-			randomForest.stdout.on("data", function (data) {
-				let rf = JSON.parse(data.toString());
-				const supportVector = spawn("python", ["svm.py"]);
-				supportVector.stdout.on("data", function (data) {
-					let svm = JSON.parse(data.toString());
-					console.log(lr);
-					console.log(rf);
-					console.log(svm);
-					csv()
-						.fromFile("amazon_review.csv")
-						.then(function (obj) {
-							res.status(200).send({
-								status: "Analysed successfully",
-								data: obj,
-								positiveCount: (lr[1] + rf[1] + svm[1]) / 3,
-								negativeCount: (lr[0] + rf[0] + svm[0]) / 3,
+	try {
+		const main = spawn("python", ["scraper.py", url]);
+		main.stdout.on("data", function (data) {
+			const logistic = spawn("python", ["logistic.py"]);
+			logistic.stdout.on("data", function (data) {
+				console.log("Pipe data from python script ...");
+				let lr = JSON.parse(data.toString());
+				const randomForest = spawn("python", ["rf.py"]);
+				randomForest.stdout.on("data", function (data) {
+					let rf = JSON.parse(data.toString());
+					const supportVector = spawn("python", ["svm.py"]);
+					supportVector.stdout.on("data", function (data) {
+						let svm = JSON.parse(data.toString());
+						console.log(lr);
+						console.log(rf);
+						console.log(svm);
+						csv()
+							.fromFile("amazon_review.csv")
+							.then(function (obj) {
+								res.status(200).send({
+									status: "Analysed successfully",
+									data: obj,
+									positiveCount: (lr[1] + rf[1] + svm[1]) / 3,
+									negativeCount: (lr[0] + rf[0] + svm[0]) / 3,
+								});
 							});
-						});
+					});
 				});
 			});
+			logistic.on("close", (code) => {
+				console.log(`child process close all stdio with code ${code}`);
+				if (code === 0) {
+					console.log(chalk.redBright(`Code 0`));
+				}
+				if (code === 1) {
+					return res
+						.status(400)
+						.send({ success: false, message: "URL couldn't be scraped" });
+				}
+			});
 		});
-		logistic.on("close", (code) => {
-			console.log(`child process close all stdio with code ${code}`);
-			if (code === 0) {
-				console.log(chalk.redBright(`Code 0`));
-			}
-			if (code === 1) {
-				return res
-					.status(400)
-					.send({ success: false, message: "URL couldn't be scraped" });
-			}
-		});
-	});
-	// main.on("close", (code) => {
-	//   console.log(chalk.redBright("Code 0 on Scraper"));
-	// });
+		// main.on("close", (code) => {
+		//   console.log(chalk.redBright("Code 0 on Scraper"));
+		// });
+	} catch (error) {
+		console.log(error);
+		res.status(400).send({ Error: "Something went wrong" });
+	}
 });
 
 app.get("/", (req, res) => {
@@ -77,24 +82,29 @@ app.post("/get-sentiment", async (req, res) => {
 	if (!text) {
 		return res.status(400).send({ message: "URL Required" });
 	}
-	const sentiment = spawn("python", ["sentiment.py", text]);
-	sentiment.stdout.on("data", function (data) {
-		console.log("Pipe data from python script ...");
-		let sent = JSON.parse(data.toString());
-		console.log(sent);
-		let st = 0;
-		s = sent[0] + sent[1] + sent[2];
-		if (s <= 1) st = 0;
-		else st = 1;
-		console.log(st);
-		res.status(200).send({
-			status: "Analysed successfully",
-			logiSentiment: sent[0],
-			rfSentiment: sent[1],
-			svmSentiment: sent[2],
-			sentiment: st,
+	try {
+		const sentiment = spawn("python", ["sentiment.py", text]);
+		sentiment.stdout.on("data", function (data) {
+			console.log("Pipe data from python script ...");
+			let sent = JSON.parse(data.toString());
+			console.log(sent);
+			let st = 0;
+			s = sent[0] + sent[1] + sent[2];
+			if (s <= 1) st = 0;
+			else st = 1;
+			console.log(st);
+			res.status(200).send({
+				status: "Analysed successfully",
+				logiSentiment: sent[0],
+				rfSentiment: sent[1],
+				svmSentiment: sent[2],
+				sentiment: st,
+			});
 		});
-	});
+	} catch (error) {
+		console.log(error);
+		res.status(400).send({ Error: "Something went wrong" });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
