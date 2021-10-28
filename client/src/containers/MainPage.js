18dc19f8e21f4b4b2bcbe52ed1732f3b9a8ee833
keshav263 <chhapariakeshav@gmail.com>
2021-10-28 17:41:30 +0530

    change landing and auth ui

--- client/src/containers/MainPage.js
+++ client/src/containers/MainPage.js
@@ -1,23 +1,132 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import Pic from "../assets/stat.png";
+import Home from "../assets/home.png";
+import { useSelector } from "react-redux";
+export default function MainPage(props) {
+	const isAuth = useSelector((state) => state.Auth.isAuth);
 
-export default function MainPage() {
 	return (
 		<Container>
 			<Header>
 				<img src={Pic} alt="Logo" />
-				<StyledButton variant="contained">Sign Up</StyledButton>
+
+				<StyledButton
+					onClick={() => {
+						if (isAuth) props.history.push("/home");
+						else props.history.push("/login");
+					}}
+					variant="contained"
+				>
+					{isAuth ? "Go to console" : "Log In"}
+				</StyledButton>
 			</Header>
+			<LinksContainer>
+				<StyledLink underline="none" href="#">
+					About
+				</StyledLink>
+				<span>/</span>
+				<StyledLink underline="none" href="#">
+					Pricing
+				</StyledLink>
+				<span>/</span>
+				<StyledLink underline="none" href="#">
+					Contact
+				</StyledLink>
+			</LinksContainer>
+			<Tag>premium panel for success</Tag>
+			<Title>
+				Manage your tasks <span> easier</span> and <span>simple</span>
+			</Title>
+			<SubTitle>
+				The easiest way to help you organise and control your task with fast
+				service
+			</SubTitle>
+			<GetStartedButton
+				variant="contained"
+				onClick={() => {
+					if (isAuth) props.history.push("/home");
+					else props.history.push("/login");
+				}}
+			>
+				Get Started
+			</GetStartedButton>
+			<HomePic alt="Home screen" src={Home}></HomePic>
 		</Container>
 	);
 }
 
+const HomePic = styled.img`
+	width: 60vw;
+	align-self: center;
+	border-radius: 1rem;
+	margin-top: 3rem;
+`;
+
+const GetStartedButton = styled(Button)`
+	background: #027f67;
+	text-transform: capitalize;
+	align-self: center;
+	border-radius: 0.4rem;
+	&:hover {
+		background: #027f67;
+		opacity: 0.8;
+	}
+`;
+
+const Title = styled.h1`
+	color: #000;
+	text-align: center;
+	width: 40vw;
+	margin: 0 auto 1rem;
+	font-weight: 400;
+	letter-spacing: 2px;
+	font-size: 4rem;
+	> span {
+		background-clip: text;
+		background: -webkit-linear-gradient(#446962, #79bda6);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+`;
+
+const SubTitle = styled.p`
+	margin: 0 auto 2rem;
+	width: 30vw;
+	text-align: center;
+	letter-spacing: 1px;
+	font-size: 0.7rem;
+`;
+
+const Tag = styled.p`
+	text-transform: uppercase;
+	color: #79bda6;
+	margin: 3rem auto 1rem;
+	width: 20vw;
+	text-align: center;
+`;
+
+const LinksContainer = styled.div`
+	display: flex;
+	width: 16vw;
+	align-items: center;
+	justify-content: space-between;
+	color: #000;
+	box-sizing: border-box;
+	margin: 0 auto;
+	transform: translateY(-150%);
+`;
+
+const StyledLink = styled(Link)`
+	color: #000;
+	font-size: 0.8rem;
+`;
+
 const StyledButton = styled(Button)`
 	background: #027f67;
 	text-transform: capitalize;
-	padding: 0 0.3rem;
+	padding: 0 0.5rem;
 	font-size: 0.7rem;
 	height: 2rem;
 	border-radius: 0.4rem;
@@ -28,8 +137,8 @@ const StyledButton = styled(Button)`
 `;
 
 const Header = styled.div`
-	padding: 2rem 3rem;
 	display: flex;
+	align-items: center;
 	justify-content: space-between;
 	> img {
 		height: 2.5rem;
@@ -37,7 +146,12 @@ const Header = styled.div`
 `;
 
 const Container = styled.div`
+	padding: 2rem 3rem;
+	display: flex;
+	flex-direction: column;
+	box-sizing: border-box;
 	height: 100vh;
 	width: 100vw;
-	background: linear-gradient(to bottom right, #eef4f3 50%, #ffffff 50%);
+	background: linear-gradient(to bottom right, #eef4f3 70%, #ffffff 50%);
+	overflow: scroll;
 `;
