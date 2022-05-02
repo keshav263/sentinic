bfc03719dbb4523e3f80b2c82601370429252dfa
keshav263 <chhapariakeshav@gmail.com>
2022-05-02 22:35:26 +0530

    change constant

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,2 +1,2 @@
-export const url = "http://72a4-203-115-66-147.ngrok.io";
-// export const url = "https://sentinic-1.herokuapp.com";
+// export const url = "http://72a4-203-115-6-147.ngrok.io";
+export const url = "https://sentinic-1.herokuapp.com";
